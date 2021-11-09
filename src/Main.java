4753af684cd217180dea49d655873334c278ba80
still-not-telling <JR212384@reigate.ac.uk>
2021-11-09 11:35:02 +0000

    reformatting

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         while (true) {
             for (int i = 0; i < 10; i++) {
-                for (int j = i; j < 10 ; j  ++){
+                for (int j = i; j < 10; j++) {
                     System.out.print(" ");
                 }
                 System.out.println("// ");
                 TimeUnit.MILLISECONDS.sleep(200);
 
             }
-            for(int k = 0 ; k < 10; k++){
+            for (int k = 0; k < 10; k++) {
                 System.out.print(" ");
-                for(int l = 0 ; l < k ; l ++){
+                for (int l = 0; l < k; l++) {
                     System.out.print(" ");
                 }
-                System.out.println("\\"+"\\");
+                System.out.println("\\" + "\\");
                 TimeUnit.MILLISECONDS.sleep(200);
             }
         }
